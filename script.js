62b60faa72641d59ccd43c975d5608df2ea35e2d
Zeeshan <zeeshanaliryk18@gmail.com>
2021-08-26 09:29:24 +0500

    Fix some issues in javascript file

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
+// Storing the themoviedb api url in the variable
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+// Storing the image path that will be used to show movie image
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+// Storing the api irl for searching the movie
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
+// getting the id's of required html elements
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
-// Get initial movies
+// Calling the getMovie function and passing the API url in it
 getMovies(API_URL)
 
+// Defining the async function that will return a promise and fetch the data of api
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
-
+    // calling the function and passing the data to show movies data into html page
     showMovies(data.results)
 }
 
+// function that will loop through each movie result and show movies on the html page
 function showMovies(movies) {
     main.innerHTML = ''
 
     movies.forEach((movie) => {
+        // getting the required data by array destructuring
         const { title, poster_path, vote_average, overview } = movie
 
         const movieEl = document.createElement('div')
@@ -40,6 +47,7 @@ function showMovies(movies) {
     })
 }
 
+// function that is passing a value (rating) and return string of color name accourding to the given condition.
 function getClassByRate(vote) {
     if(vote >= 8) {
         return 'green'
@@ -50,23 +58,14 @@ function getClassByRate(vote) {
     }
 }
 
+
+// performing an input event that will get the search term from user input and call the same function getMovies() but this time it will pass the SEARCH API with the user input value.
 search.addEventListener("input",()=>{
     const searchTerm = search.value
     if(searchTerm && searchTerm !== '') {
         getMovies(SEARCH_API + searchTerm)
-        // search.value = ''
     }
      else {
-        window.location.reload()
-    }
-})
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const searchTerm = search.value
-    if(searchTerm && searchTerm !== '') {
-        getMovies(SEARCH_API + searchTerm)
-        search.value = ''
-    } else {
         window.location.reload()
     }
 })
